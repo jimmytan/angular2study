58287427d1db8c62050786e5c4ab83b80c6d1fc3
jimmytan <jimmytan119@hotmail.com>
2016-12-29 00:48:14 -0500

    start form app

---
+++ app/forms/app.module.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by wenjuntan on 2016-12-29.
+ */
+import {
+    FormsModule,
+    ReactiveFormsModule
+}   from '@angular/forms'
+import {NgModule} from "@angular/core";
+@NgModule({
+    declarations
+})
